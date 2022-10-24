70cb81441d2d61d3347d058bdc87bf417066d426
Utkarsh721 <utkarshrai2121>
2022-10-24 22:16:11 +0530

    add this

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[2]);
-li[2].style.backgroundColor= 'green'
-
-for(var i=0 ; i<li.length;i++){
-items[i].style.fontWeight = 'bold'
-}
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[4]);
+// li[4].style.backgroundColor= 'green'
+// li[4].style.fontWeight='bold'
 
 
+var items=document.getElementsByClassName('recovery');
+console.log(items);
+console.log(items[4]);
+items[4].style.backgroundColor= 'green'
+items[4].style.fontWeight='bold'
+items[4].textContent='Hello'
 
 
 
