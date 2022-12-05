e475fe1961bedc139cf3a2172fc1d68a8b818ff8
Utkarsh721 <utkarshrai2121>
2022-12-05 20:51:37 +0530

    page dynamic task 11

---
+++ pgdytask11.js
@@ -0,0 +1,66 @@
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    const name = event.target.username.value;
+    const email = event.target.emailId.value;
+  
+    const obj = {
+        name, 
+        email,
+  
+    }
+
+    
+    axios.post("https://crudcrud.com/api/bc3ad19cf6ab48059c45673a17d07c4c/appintmentData", obj)
+       .then((respone) => {
+        showNewUserOnScreen(respone.data)
+        console.log(respone);
+       })
+       .catch((err) => {
+        document.body.innerHTML = document.body.innerHTML + "<h4>Somthing went wrong</h4>"
+        console.log(err);
+       })
+
+
+    // localStorage.setItem(obj.email, JSON.stringify(obj));
+    // showNewUserOnScreen(obj)
+  }
+  window.addEventListener('DOMContentLoaded', (event) => {
+    Object.keys(localStorage).forEach(key => {
+        const user = JSON.parse(localStorage.getItem(key))
+        showNewUserOnScreen(user)
+    })
+  });
+  
+  function showNewUserOnScreen(user) {
+    // console.log(localStorage.getItem(user.email));
+    if(localStorage.getItem(user.email)!== null) {
+        removeUserFromScreen(user.email);
+    }
+    const parentNode  = document.getElementById('ListOfUser');
+    const childHTML = `<li id=${user.email}> ${user.name} - ${user.email}
+    
+    <button onclick=editUserDetails('${user.name}','${user.email}')>Edit Details</button>
+    <button onclick=deleteUser('${user.email}')> Delete User</button>
+    </li>`
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+  }
+  
+  function deleteUser(emailId) {
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+  }
+  function removeUserFromScreen(emailId) {
+    const parentNode = document.getElementById('ListOfUser');
+    const childNodeToBeDeleted = document.getElementById(emailId);
+    if(childNodeToBeDeleted) {
+        parentNode.removeChild(childNodeToBeDeleted);
+    }
+  }
+  function editUserDetails ( username, emailId, phoneNumber) {
+    document.getElementById('username').value;
+    document.getElementById('emailId').value ;
+  
+  
+    deleteUser(emailId);
+
+  }
